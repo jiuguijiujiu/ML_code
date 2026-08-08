9b39bcb0d171eab02ab188c072d8be4d5588b425
jiuguijiujiu <1054981878@qq.com>
2026-08-08 16:06:28 +0800

    2026/8/8_0

--- 练习.py
+++ 练习.py
@@ -1,38 +1,36 @@
 import pandas as pd
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import classification_report
-import matplotlib.pyplot as plt
-from sklearn.tree import plot_tree
 
-# 提取数据
-data = pd.read_csv('./data/train.csv')
+data = pd.read_csv('./data/wine0501.csv')
 # data.info()
+# print(data['Class label'].unique())
 
-# 数据预处理
-data['Age'].fillna(data['Age'].mean(),inplace = True)
-data = pd.get_dummies(data, columns = ['Sex'])
-data.drop(columns = 'Sex_female', inplace = True)
-# data.info()
+data = data[data['Class label'] != 3]
+# print(data.head())
+# print(data['Class label'].unique())
 
-# 特征工程
-x = data[['Pclass', 'Age', 'Sex_male']]
-y = data['Survived']
-x.info()
+x = data[['Alcohol', 'Hue']]        # 酒精与色泽
+y = data['Class label']
+
+ls = LabelEncoder()
+y = ls.fit_transform(y)
+# print(y)
+
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
 
-# 模型训练
-estimator = DecisionTreeClassifier(max_depth = 10)
-estimator.fit(x_train, y_train)
+estimator1 = DecisionTreeClassifier(max_depth = 3)
+estimator1.fit(x_train, y_train)
+y_pred1 = estimator1.predict(x_test)
+print(y_pred1)
+print(accuracy_score(y_test, y_pred1))
+print('-' * 50)
 
-# 模型预测
-y_pre = estimator.predict(x_test)
-print(y_pre)
-
-# 模型评估
-print(classification_report(y_test, y_pre))
-
-# 绘制
-plt.figure(figsize = (30, 30))
-plot_tree(estimator, max_depth = 10, filled = True)
-plt.show()
+estimator2 = AdaBoostClassifier(estimator1, n_estimators = 100, learning_rate = 0.1)
+estimator2.fit(x_train, y_train)
+y_pred2 = estimator2.predict(x_test)
+print(y_pred2)
+print(accuracy_score(y_test, y_pred2))
